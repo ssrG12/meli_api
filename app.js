7ea68a8dd12a22f5227bb26afa45bbcfcdbb4f07
ssrG12 <santiagosrueda@gmail.com>
2024-07-01 00:54:23 -0500

    Ajuste cors

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+import cors from 'cors'
 import express from 'express'
 import searchByQuery from './routes/searchByQuery.js'
 import detailProduct from './routes/detailProduct.js'
@@ -5,8 +6,11 @@ import detailProduct from './routes/detailProduct.js'
 const port = 3001
 const app = express()
 
+app.use(cors())
+
 app.use('/api/items', searchByQuery)
 app.use('/api/items', detailProduct)
+
 
 app.use((err, req, res, next) => {
   res.status(err.status || 500).json({
